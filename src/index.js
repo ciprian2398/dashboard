96b257ed50f3a1121ea261f42314d2e5943e0386
ciprian <cnicuta@fusionworks.md>
2022-08-04 19:27:24 +0300

    up until 41:00

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 
+import './index.css'; //tailgate
 import App from './App';
 
 ReactDOM.render(<App/>, document.getElementById('root')); //hook react application to root div
